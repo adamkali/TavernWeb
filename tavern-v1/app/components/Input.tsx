9e5383b9e3bdab08b03ba80bf9aa24c5d3d4711d
adamkali <adamkali@outlook.com>
2022-12-10 23:41:10 -0500

    push

--- tavern-v1/app/components/Input.tsx
+++ tavern-v1/app/components/Input.tsx
@@ -1,41 +1,67 @@
-// Create an input component
+'use client';
 
 export default function Input({ props }: { props: any }): JSX.Element {
-    const bgColor = () => {
-        switch (props.bgColor) {
+    const bgColor = (p: string) => {
+        switch (p) {
             case 'normal':
-                return 'bg-vaporwave-normal-1';
+                return 'input-container input--normal animate-light-flicker';
             case 'accent':
-                return 'bg-vaporwave-accent-2';
+                return 'input-container input--accent animate-light-flicker';
             case 'dark':
-                return 'bg-vaporwave-dark-2';
+                return 'input-container input--dark animate-light-flicker';
             default:
-                return 'bg-vaporwave-normal-1';
+                return 'input-container input--normal animate-light-flicker';
         }
     };
 
-    const txColor = () => {
-        switch (props.txColor) {
+    const txColor = (p: string) => {
+        switch (p) {
             case 'primary':
                 return 'text-vaporwave-primary';
             case 'accent':
                 return 'text-vaporwave-secodary';
             case 'dark':
-                return 'text-vaporwave-dark-1';
+                return 'text-vaporwave-tertiary';
             default:
                 return 'text-vaporwave-primary';
         }
     };
 
+    console.log(txColor(props.txColor), bgColor(props.bgColor));
+
     return (
         <div
-            className={`card card-body mb-4 ${bgColor + ' ' + txColor} animate-light-flicker`}
+            className={`group relative w-full flex my-16 group-hover:scale-y-125`}
         >
-            <div>{props.label}</div>
-            <div className="text-vaporwave-secondary">
-                <div className="w-1/5">{props.icon}</div>
-                <div className="w-4/5">
+            <div
+                className={`
+                    ${bgColor(props.bgColor)}
+                    absolute w-full h-20 
+                    inset-0 input--accent
+                    `}
+            >
+                <div
+                    className="w-1/5 text-2xl 
+                                justify-start 
+                                align-middle mt-6 pl-8
+                                group-hover:scale-150"
+                >
+                    {props.icon}
+                </div>
+            </div>
+            <div className="absolute w-full inset-0 z-10 ">
+                <div className=" font-bold flex mx-16 justify-start">
+                    {props.label}
+                </div>
+                <div
+                    className={`flex flex-1 flex-row 
+                        w-full text-2xl align-bottom 
+                        mb-8 ${txColor(props.txColor)}
+                        group-hover:scale-150`}
+                >
+                    <div className="w-2/5"></div>
                     <input
+                        className="w-3/5 input-holder h-full"
                         type={props.type}
                         placeholder={props.placeholder}
                         value={props.value}
--- tavern-v1/app/welcome/login/LoginFom.tsx
+++ tavern-v1/app/welcome/login/LoginFom.tsx
@@ -1,9 +1,3 @@
-// create a form:
-// it should have a username input
-// it should have a password input
-// it should have a button that sends a LoginRequest to the server
-// there should be a LoginRequest That is changed based on a use memo
-
 'use client';
 
 // ----- React  Imports ----- //
@@ -15,7 +9,9 @@ import { BsFillUnlockFill } from '@react-icons/all-files/bs/BsFillUnlockFill';
 import { FaCheck } from '@react-icons/all-files/fa/FaCheck';
 
 // ----- Tavern Imports ----- //
-import TavernProfile, { TavernModels } from '../../../providers/receivers';
+import TavernProfile, {
+    TavernModels,
+} from '../../../providers/receivers';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import * as Authenticate from '../../../providers/receivers/Authenticate';
@@ -39,58 +35,74 @@ export default function LoginForm(): JSX.Element {
     }, [username, password]);
 
     const onSubmit = () => {
-        TavernProfile.login({ body: token }).then((resp) => {
-            resp.check() ? resp.run((r) => {
-                Authenticate.setToken(r);
-                alert('Logged in!');
-            }) : console.log(resp.error());
-        },
-        (err) => {
-            console.log(err);
-        });
+        TavernProfile.login({ body: token }).then(
+            (resp) => {
+                resp.check()
+                    ? resp.run((r) => {
+                          Authenticate.setToken(r);
+                          alert('Logged in!');
+                      })
+                    : console.log(resp.error());
+            },
+            (err) => {
+                console.log(err);
+            }
+        );
     };
 
     return (
-        <div className="flex flex-1 flex-col justify-end">
-            <div className='grid grid-rows-3 mb-16 px-8'>
-                <Input
-                    props={{
-                        label: 'Username',
-                        icon: <BsFillPersonFill />,
-                        type: 'email',
-                        placeholder: 'HeroMan100',
-                        value: username,
-                        onChange: (e: any) =>
-                            setUsername(e.target.value),
-                    }}
-                />
-                <Input
-                    props={{
-                        label: 'Password',
-                        type: 'password',
-                        placeholder: '********',
-                        value: password,
-                        onChange: (e: any) =>
-                            setPassword(e.target.value),
-                    }}
-                />
+        <div className="flex flex-row w-full my-16 px-8">
+            <div className="h-full w-full"></div>
+            <div className="grid grid-rows-3 w-full">
+                <div className="flex flex-1 flex-col justify-end px-8">
+                    <Input
+                        props={{
+                            label: 'Username',
+                            icon: <BsFillPersonFill />,
+                            type: 'email',
+                            placeholder: 'HeroMan100',
+                            value: username,
+                            txColor: 'secondary',
+                            bgColor: 'accent',
+                            onChange: (e: any) =>
+                                setUsername(e.target.value),
+                        }}
+                    />
+                    <Input
+                        props={{
+                            label: 'Password',
+                            type: 'password',
+                            icon: <BsFillUnlockFill />,
+                            placeholder: '********',
+                            value: password,
+                            txColor: 'secondary',
+                            bgColor: 'accent',
+                            onChange: (e: any) =>
+                                setPassword(e.target.value),
+                        }}
+                    />
+                </div>
+                <div className=" px-8 justify-end mt-16">
+                    <Button
+                        bgColor="accent"
+                        txColor="primary"
+                        icon={<FaCheck />}
+                        hover={true}
+                        onClick={onSubmit}
+                    >
+                        Login
+                    </Button>
+                </div>
+                <p>
+                    Don't have an account? Then click{' '}
+                    <Link
+                        href="/welcome/signup"
+                        className="text-vaporwave-secondary secondary-shine"
+                    >
+                        here!
+                    </Link>
+                </p>
             </div>
-            <div className='px-8'>
-                <Button
-                    bgColor="accent"
-                    txColor="primary"
-                    icon={<FaCheck />}
-                    hover={true}
-                    onClick={onSubmit}
-                >
-                    Login
-                </Button>
-            </div>
-            <p>Don't have an account? Then click 
-            <Link
-                href="/welcome/signup"
-                className='text-vaporwave-secondary secondary-shine'
-            >here!</Link></p>
         </div>
     );
 }
